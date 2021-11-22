6c2e73eb48ae9a18630852a38078d0c3a7f9148a
ahngbeom <bbu0704@gmail.com>
2021-11-22 13:10:26 +0900

    The starting point of the meal was set as the reference point.

--- philo/srcs/eating.c
+++ philo/srcs/eating.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 11:27:14 by bahn              #+#    #+#             */
-/*   Updated: 2021/11/22 00:49:48 by bahn             ###   ########.fr       */
+/*   Updated: 2021/11/22 13:01:23 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ int eating(t_philo *philo)
     pthread_mutex_lock(&philo->died_mutex);
     ft_print(philo->table, philo->id, "is eating");
     philo->last_eat_time = time_ms();
+    philo->eat_count++;
     pthread_mutex_unlock(&philo->died_mutex);
     while (time_ms() - philo->last_eat_time <= philo->table->time_to_eat)
         usleep(1000);
-    // philo->last_eat_time = time_ms();
-    philo->eat_count++;
     pthread_mutex_unlock(&philo->table->fork_mutex[philo->id - 1]);
     pthread_mutex_unlock(&philo->table->fork_mutex[philo->id % philo->table->number_of_philos]);
     return (philo->table->died_philos);
