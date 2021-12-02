e8f8518879594790fe6a45747559438818d6ab4d
ahngbeom <bbu0704@gmail.com>
2021-12-02 16:30:16 +0900

    Really last memory leak check => SUCCESS

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:29:09 by bahn              #+#    #+#             */
-/*   Updated: 2021/12/01 16:32:25 by bahn             ###   ########.fr       */
+/*   Updated: 2021/12/02 15:20:49 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ int	main(int argc, char *argv[])
 	if (argc < 5 || argc > 6)
 		ft_exception("Insufficient or excessive argument are present");
 	table = table_setting(argc, argv);
-	if (argc == 5)
-		table->philos = philosophers_init(table);
-	else
-		table->philos = philosophers_init(table);
+	if (table == NULL)
+		ft_error(table, "malloc error");
+	table->philos = philosophers_init(table);
 	if (table->philos == NULL)
-		ft_error(table, "malloc");
+		ft_error(table, "malloc error");
 	philosophers_doing(table);
 	ft_free(table);
 	return (0);
